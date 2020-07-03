94de2b7aa9ca9c7cb9e381910ed01f3dbceb307f
Rodrigo Tufiño <rtufino@ups.edu.ec>
2020-07-03 13:01:01 -0500

    Usuarios RestServer completo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config')
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,37 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+// incluir rutas de /usuario
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', (req, res) => {
-    res.json("get Usuario");
-});
-
-app.post('/usuario', (req, res) => {
-
-    let body = req.body
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
+// Conectar con MongoDB
+mongoose.connect(process.env.URLDB, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
+    (err, res) => {
+        if (err) throw err;
+        console.log("Base de Datos ONLINE!");
     });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json("delete Usuario");
-});
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando en el puerto", process.env.PORT);
